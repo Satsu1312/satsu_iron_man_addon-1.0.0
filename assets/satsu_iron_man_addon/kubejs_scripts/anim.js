fa96caccde9d92e63ec52ac9e26e033e1367aff1
Satsu1312 <minion1312@hotmail.com>
2024-12-22 19:03:34 -0600

    Superior pose

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -99,3 +99,79 @@ PalladiumEvents.registerAnimations((event) => {
     }
   });
 });
+PalladiumEvents.registerAnimations((event) => {
+  event.register("satsu_iron_man_addon/im_superior", 10, (builder) => {
+    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+    const progress = animationUtil.getAnimationTimerAbilityValue(
+      builder.getPlayer(),
+      "satsu_iron_man_addon:kube_animations",
+      "test",
+      builder.getPartialTicks()
+    );
+
+    // only apply animation if progress is above 0!
+    if (progress > 0.0) {
+      const halfPi = 1.57079632679;
+
+      // if first person
+      if (builder.isFirstPerson()) {
+        builder
+          .get("left_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      }
+      if (builder.isFirstPerson()) {
+        builder
+          .get("right_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      } else {
+        // third person animations
+        builder
+          .get("left_arm")
+          .setXRotDegrees(-75.2219)
+          .setYRotDegrees(36.3885)
+          .setZRotDegrees(-3.0067)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-75.2219)
+          .setYRotDegrees(-36.3885)
+          .setZRotDegrees(-3.0067)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("left_leg")
+          .setXRotDegrees(-45)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("right_leg")
+          .setXRotDegrees(-66.8516)
+          .setYRotDegrees(-10.2124)
+          .setZRotDegrees(-7.2468)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("chest")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+        builder
+          .get("body")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+        builder.get("chest").animate("InOutCubic", progress);
+      }
+    }
+  });
+});
