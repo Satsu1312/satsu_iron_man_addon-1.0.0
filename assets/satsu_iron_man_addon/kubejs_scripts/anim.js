aeb4a75bf78233bc3c7e4394eda0b131e24c487c
Satsu1312 <minion1312@hotmail.com>
2025-01-19 16:45:31 -0600

    Blooded armor anim test

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -747,3 +747,64 @@ PalladiumEvents.registerAnimations((event) => {
     }
   );
 });
+PalladiumEvents.registerAnimations((event) => {
+  event.register(
+    "satsu_iron_man_addon/colocacion_de_armadura.6.blooded.mark.pose",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:kube_animations",
+        "blooded_activaded_anim",
+        builder.getPartialTicks()
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(49)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(30)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-40)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(-30)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("left_arm")
+            .setXRotDegrees(-70.0709)
+            .setYRotDegrees(31.772)
+            .setZRotDegrees(25.7009)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-67.5709)
+            .setYRotDegrees(-31.772)
+            .setZRotDegrees(-25.7009)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
+});
