ee302df4c90d7306968a1263480c8492b21db709
Satsu1312 <minion1312@hotmail.com>
2025-01-29 15:54:08 -0600

    Test

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -1055,6 +1055,65 @@ PalladiumEvents.registerAnimations((event) => {
       }
     }
   );
+  event.register(
+    "satsu_iron_man_addon/colocacion_de_armadura.7.mark.47.part.3",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:kube_animations",
+        "mark_47_anim.3",
+        builder.getPartialTicks()
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(49)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(30)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-85)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(-30)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("left_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(-75)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder
+            .get("right_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(75)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
 });
 PalladiumEvents.registerAnimations((event) => {
   event.register(
