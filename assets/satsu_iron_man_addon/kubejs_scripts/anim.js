247e2aefacfe32ad8a74027b6ee008db44577811
Satsu1312 <minion1312@hotmail.com>
2025-02-14 13:52:19 -0600

    Mark 1873 smoke abilities

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -1618,3 +1618,110 @@ PalladiumEvents.registerAnimations((event) => {
     }
   });
 });
+PalladiumEvents.registerAnimations((event) => {
+  event.register("satsu_iron_man_addon/clap_smoke_hands.1", 10, (builder) => {
+    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+    const progress = animationUtil.getAnimationTimerAbilityValue(
+      builder.getPlayer(),
+      "satsu_iron_man_addon:kube_animations",
+      "mark.1873.clap_smoke_hands",
+      builder.getPartialTicks()
+    );
+
+    // only apply animation if progress is above 0!
+    if (progress > 0.0) {
+      const halfPi = 1.57079632679;
+
+      // if first person
+      if (builder.isFirstPerson()) {
+        builder
+          .get("left_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      }
+      if (builder.isFirstPerson()) {
+        builder
+          .get("right_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      } else {
+        // third person animations
+        builder
+          .get("left_arm")
+          .setXRotDegrees(-90)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .moveX(0)
+          .moveY(0)
+          .moveZ(0)
+          .animate("InOutCubic", progress);
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-90)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .moveX(0)
+          .moveY(0)
+          .moveZ(0)
+          .animate("InOutCubic", progress);
+        builder;
+      }
+    }
+  });
+  event.register("satsu_iron_man_addon/clap_smoke_hands.2", 10, (builder) => {
+    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+    const progress = animationUtil.getAnimationTimerAbilityValue(
+      builder.getPlayer(),
+      "satsu_iron_man_addon:kube_animations",
+      "mark.1873.clap_smoke_hands.2",
+      builder.getPartialTicks()
+    );
+
+    // only apply animation if progress is above 0!
+    if (progress > 0.0) {
+      const halfPi = 1.57079632679;
+      // if first person
+      if (builder.isFirstPerson()) {
+        builder
+          .get("left_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      }
+      if (builder.isFirstPerson()) {
+        builder
+          .get("right_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      } else {
+         // third person animations
+          builder
+            .get("left_arm")
+            .setXRotDegrees(-90)
+            .setYRotDegrees(25)
+            .setZRotDegrees(0)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-90)
+            .setYRotDegrees(-25)
+            .setZRotDegrees(0)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder;
+      }
+    }
+  });
+});
