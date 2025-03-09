0bf546614d940507492ae216ae51783eef1bf0e1
Satsu1312 <minion1312@hotmail.com>
2025-03-09 17:38:41 -0600

    Update anim.js

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -1602,9 +1602,9 @@ PalladiumEvents.registerAnimations((event) => {
       if (builder.isFirstPerson()) {
         builder
           .get("right_arm")
-          .setXRotDegrees(49)
-          .setYRotDegrees(-30)
-          .setZRotDegrees(-30)
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
           .animate("InOutCubic", progress);
       } else {
         // third person animations
