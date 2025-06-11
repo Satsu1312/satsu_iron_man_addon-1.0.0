2ac64ad0d7c1e8a27e55cc2b610dc09a0b6d8b91
Satsu1312 <minion1312@hotmail.com>
2025-06-11 14:03:32 -0600

    moved shield animation

--- assets/satsu_iron_man_addon/kubejs_scripts/anim.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/anim.js
@@ -1563,48 +1563,6 @@ PalladiumEvents.registerAnimations((event) => {
   });
 });
 PalladiumEvents.registerAnimations((event) => {
-  event.register("satsu_iron_man_addon/shield_put_animation", 10, (builder) => {
-    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
-    const progress = animationUtil.getAnimationTimerAbilityValue(
-      builder.getPlayer(),
-      "satsu_iron_man_addon:kube_animations",
-      "shield_put_animation",
-      builder.getPartialTicks()
-    );
-
-    // only apply animation if progress is above 0!
-    if (progress > 0.0) {
-      const halfPi = 1.57079632679;
-
-      // if first person
-      if (builder.isFirstPerson()) {
-        builder
-          .get("left_arm")
-          .setXRotDegrees(0)
-          .setYRotDegrees(0)
-          .setZRotDegrees(0)
-          .animate("InOutCubic", progress);
-      }
-      if (builder.isFirstPerson()) {
-        builder
-          .get("right_arm")
-          .setXRotDegrees(-80)
-          .setYRotDegrees(0)
-          .setZRotDegrees(-100)
-          .animate("InOutCubic", progress);
-      } else {
-        // third person animations
-        builder
-          .get("right_arm")
-          .setXRotDegrees(-90)
-          .setYRotDegrees(-40)
-          .animate("InOutCubic", progress);
-        builder;
-      }
-    }
-  });
-});
-PalladiumEvents.registerAnimations((event) => {
   event.register(
     "satsu_iron_man_addon/no_swim_mark_animation.mark.37",
     10,
