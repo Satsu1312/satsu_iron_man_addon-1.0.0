a640342f60ba4fede3f0667b581ebeb7f424c446
Satsu1312 <minion1312@hotmail.com>
2025-07-03 17:01:14 -0600

    Igor shield

---
+++ assets/satsu_iron_man_addon/kubejs_scripts/marks/mark_38/shield/main.js
@@ -0,0 +1,43 @@
+// Register custom animations
+PalladiumEvents.registerAnimations((event) => {
+  event.register("satsu_iron_man_addon/marks/mark_38/shield_put_animation", 10, (builder) => {
+    // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+    const progress = animationUtil.getAnimationTimerAbilityValue(
+      builder.getPlayer(),
+      "satsu_iron_man_addon:marks/mark_38/main",
+      "shield_put_animation",
+      builder.getPartialTicks()
+    );
+
+    // only apply animation if progress is above 0!
+    if (progress > 0.0) {
+      const halfPi = 1.57079632679;
+
+      // if first person
+      if (builder.isFirstPerson()) {
+        builder
+          .get("left_arm")
+          .setXRotDegrees(0)
+          .setYRotDegrees(0)
+          .setZRotDegrees(0)
+          .animate("InOutCubic", progress);
+      }
+      if (builder.isFirstPerson()) {
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-80)
+          .setYRotDegrees(0)
+          .setZRotDegrees(-100)
+          .animate("InOutCubic", progress);
+      } else {
+        // third person animations
+        builder
+          .get("right_arm")
+          .setXRotDegrees(-90)
+          .setYRotDegrees(-40)
+          .animate("InOutCubic", progress);
+        builder;
+      }
+    }
+  });
+});
