26a46dc1d957901cd227f8e65b1c54931f3929b9
Satsu1312 <minion1312@hotmail.com>
2025-07-15 16:51:18 -0600

    Add ability per tick

--- addon/satsu_iron_man_addon/kubejs_scripts/new_ability.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/new_ability.js
@@ -9,7 +9,7 @@ StartupEvents.registry("palladium:abilities", (event) => {
 
     // Documentation description
     .documentationDescription(
-      "Increases a player's scoreboard by a specific amount."
+      "Add a specific power to the player when the ability is active."
     )
     .addProperty(
       "power_path",
@@ -42,7 +42,7 @@ StartupEvents.registry("palladium:abilities", (event) => {
 
     // Documentation description
     .documentationDescription(
-      "Increases a player's scoreboard by a specific amount."
+      "Remove a specific power from the player when the ability is active."
     )
     .addProperty(
       "power_path",
@@ -64,4 +64,46 @@ StartupEvents.registry("palladium:abilities", (event) => {
         palladium.superpowers.removeSuperpower(entity, `${power_path}:${powerid}`);
       }
     });
+});
+StartupEvents.registry("palladium:abilities", (event) => {
+  // ID of the ability will be: 'kubejs:satsu_iron_man_addon/add_power_per_tick'
+  event
+    .create("satsu_iron_man_addon:add_power_per_tick")
+
+    // Preset icon, can also be changed individually in the power json
+    .icon(palladium.createItemIcon("minecraft:bread"))
+
+    // Documentation description
+    .documentationDescription(
+      "Add a specific power to the player when the ability is active and remove it when the ability is no longer active"
+    )
+    .addProperty(
+      "power_path",
+      "string",
+      "palladium",
+      "Path of the power to add"
+    )
+    .addProperty(
+      "powerid",
+      "string",
+      "test_power",
+      "ID of the power to add"
+    )
+    // Handler for what happens during EVERY tick of the ability being active, make sure to check the 'enabled' parameter
+    .tick((entity, entry, holder, enabled) => {
+      if (enabled) {
+        const power_path = entry.getPropertyByName("power_path");
+        const powerid = entry.getPropertyByName("powerid");
+        palladium.superpowers.addSuperpower(entity, `${power_path}:${powerid}`);
+      }
+    })
+
+        // Handler for what happens during the LAST tick of the ability being active
+        .lastTick((entity, entry, holder, enabled) => {
+      if (enabled) {
+        const power_path = entry.getPropertyByName("power_path");
+        const powerid = entry.getPropertyByName("powerid");
+        palladium.superpowers.removeSuperpower(entity, `${power_path}:${powerid}`);
+      }
+        });
 });
