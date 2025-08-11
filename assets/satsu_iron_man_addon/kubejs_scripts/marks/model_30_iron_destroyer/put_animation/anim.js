ca429e8873924858c9bd8412d57318eb74c1f649
Satsu1312 <minion1312@hotmail.com>
2025-08-11 15:55:03 -0600

    More animations test

---
+++ assets/satsu_iron_man_addon/kubejs_scripts/marks/model_30_iron_destroyer/put_animation/anim.js
@@ -0,0 +1,105 @@
+// Register custom animations
+PalladiumEvents.registerAnimations((event) => {
+  event.register(
+    "satsu_iron_man_addon/colocacion_de_armadura.7.model_30_iron_destroyer.part.1",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:marks/model_30_iron_destroyer/main",
+        "mark.50.anim",
+        builder.getPartialTicks(),
+        0,
+        3
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(49)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(30)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-85)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(-30)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-85)
+            .setYRotDegrees(0)
+            .setZRotDegrees(0)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
+  event.register(
+    "satsu_iron_man_addon/colocacion_de_armadura.7.model_30_iron_destroyer.part.2",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:marks/model_30_iron_destroyer/main",
+        "mark.50.anim",
+        builder.getPartialTicks(),
+        4,
+        6
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(49)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(30)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-85)
+            .setYRotDegrees(-30)
+            .setZRotDegrees(-30)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-80.7517)
+            .setYRotDegrees(-57.1594)
+            .setZRotDegrees(-7.7901)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder.get("chest").animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
+});
