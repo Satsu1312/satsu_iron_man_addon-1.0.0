29d86d8c13db079ba1cbb0c0883cb65247b19134
Satsu1312 <minion1312@hotmail.com>
2025-08-16 18:56:32 -0600

    Test

--- addon/satsu_iron_man_addon/kubejs_scripts/property_abilitie.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/property_abilitie.js
@@ -55,3 +55,31 @@ StartupEvents.registry("palladium:abilities", (event) => {
       }
     });
 });
+StartupEvents.registry("palladium:abilities", (event) => {
+  // ID of the ability will be: 'kubejs:satsu_iron_man_addon/add_power'
+  event
+    .create("satsu_iron_man_addon:set_property_first_tick")
+
+    // Preset icon, can also be changed individually in the power json
+    .icon(palladium.createItemIcon("minecraft:bread"))
+
+    // Documentation description
+    .documentationDescription(
+      "Add,set or remove a specific power to the player when the ability is active."
+    )
+    .addProperty(
+      "name",
+      "string",
+      "satsu_nano_weapons",
+      "Name of the Custom property, you need them in assets and addon folder"
+    )
+    .addProperty("number", "integer", 1, "number of the amount in the property")
+    // Handler for what happens during EVERY tick of the ability being active, make sure to check the 'enabled' parameter
+    .firstTick((entity, entry, holder, enabled) => {
+      if (enabled) {
+        const name = entry.getPropertyByName("name");
+        const number = entry.getPropertyByName("number");
+        palladium.setProperty(entity, name, number);
+      }
+    });
+});
