4d71cb4fe2393db358edc793f12aef0903861b86
Satsu1312 <minion1312@hotmail.com>
2025-08-25 00:20:31 -0600

    reroute

--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -69,3 +69,10 @@ PalladiumEvents.registerProperties((event) => {
     event.registerProperty("satsu_iron_man_addon_repulser", "string", "repulser");
   }
 });
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_reroute", "string", "false");
+  }
+});
