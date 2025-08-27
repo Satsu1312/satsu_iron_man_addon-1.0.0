60c7863275004ead8ed1d069aeb513d77f9fc891
Satsu1312 <minion1312@hotmail.com>
2025-08-27 13:10:28 -0600

    trail option

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -83,3 +83,10 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
       event.registerProperty('satsu_iron_man_addon_enable_night_vision', 'string', "false");
     }
 });
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_addon_trail', 'string', "true");
+    }
+});
--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -83,3 +83,10 @@ PalladiumEvents.registerProperties((event) => {
     event.registerProperty("satsu_iron_man_addon_enable_night_vision", "string", "false");
   }
 });
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_trail", "string", "true");
+  }
+});
