8d165229cfc2a301afbb36e95fe6935c98a88891
Satsu1312 <minion1312@hotmail.com>
2025-09-12 15:09:58 -0600

    New sonic effect

---
+++ addon/satsu_iron_man_addon/kubejs_scripts/sounds.js
@@ -0,0 +1,42 @@
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:rivals_iron_man_blast_mcu");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:rivals_iron_man_steampower_blast");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:new_blast");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:new_mask_sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:nanomask");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.shotgun.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.mark.5.put.sound");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.repulsor");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:nano_weapons");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.holo_shield");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:new_beam");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:red_laser");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.armor.mark.46.mask.1");
+});
+StartupEvents.registry("sound_event", (event) => {
+  event.create("satsu_iron_man_addon:entity.effect.dampened_sonic");
+});
