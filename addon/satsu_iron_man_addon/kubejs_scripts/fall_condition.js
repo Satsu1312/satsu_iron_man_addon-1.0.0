c8b44db8b33e530936f1f00e63095618b3674846
Satsu1312 <minion1312@hotmail.com>
2025-09-17 09:26:53 -0600

    Create fall_condition.js

---
+++ addon/satsu_iron_man_addon/kubejs_scripts/fall_condition.js
@@ -0,0 +1,13 @@
+StartupEvents.registry("palladium:condition_serializer", (event) => {
+  event
+    .create("satsu_iron_man_addon:is_falling")
+    .addProperty("distance", "integer", 1, "distance of the blocks falling")
+    .test((entity, properties) => {
+      let blocks = properties.get("distance");
+      if (entity.fallDistance > blocks) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+});
