3ad4f3dee20617acbc11ff687ae238100c25e57d
Satsu1312 <minion1312@hotmail.com>
2025-09-21 23:03:50 -0600

    Update command.js

--- data/satsu_iron_man_addon/kubejs_scripts/command.js
+++ data/satsu_iron_man_addon/kubejs_scripts/command.js
@@ -8,13 +8,16 @@ ServerEvents.commandRegistry((event) => {
   // Command with arguments
   event.register(
     Commands.literal("satsu_set_flight_speed")
-      .executes((ctx) => {
-        // Get the player from context
-        const player = ctx.source.player;
-        // Send confirmation message
-        palladium.setProperty(player, "satsu_iron_man_flight_speed_choose", 0);
+        .then(Commands.argument("number", Arguments.INTEGER.create(event))
+            .executes((ctx) => {
+                // Get the player from context
+                const player = ctx.source.player;
+                const value = Arguments.INTEGER.getResult(ctx, 'number');
+                // Send confirmation message
+                palladium.setProperty(player, "satsu_iron_man_flight_speed_choose", value);
 
-        return 1;
-      })
+                return value;
+            })
+        )
   );
 });
