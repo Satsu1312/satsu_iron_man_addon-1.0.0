0690e01b9b90a1cadf66ee0d40b563e0892fa403
Satsu1312 <minion1312@hotmail.com>
2025-10-05 17:32:33 -0600

    horizon_lock

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -185,6 +185,13 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
   // Only register for players
   if (event.getEntityType() === "minecraft:player") {
       // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_hud_horizon_lock', 'string', "on");
+    }
+});
+PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+      // Arguments: Key of the property, type of the property, default/starting value
       event.registerProperty('satsu_iron_man_mark_80_drone', 'string', "off");
     }
 });
--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -201,6 +201,13 @@ PalladiumEvents.registerProperties((event) => {
   // Only register for players
   if (event.getEntityType() === "minecraft:player") {
     // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_hud_horizon_lock", "string", "on");
+  }
+});
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
     event.registerProperty("satsu_iron_man_mark_80_drone", "string", "off");
   }
 });
