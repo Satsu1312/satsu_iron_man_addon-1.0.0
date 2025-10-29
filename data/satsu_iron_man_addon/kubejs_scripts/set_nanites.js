670ecaf0f0b14c7b4b0364e62b0b0e7f33d15ef9
Satsu1312 <minion1312@hotmail.com>
2025-10-28 20:22:45 -0600

    Create set_nanites.js

---
+++ data/satsu_iron_man_addon/kubejs_scripts/set_nanites.js
@@ -0,0 +1,23 @@
+// KubeJS Command Registration Template
+
+// Register a custom command
+ServerEvents.commandRegistry((event) => {
+  const { commands: Commands, arguments: Arguments } = event;
+
+  // Register a basic command
+  // Command with arguments
+  event.register(
+    Commands.literal("satsu_set_nanites")
+        .then(Commands.argument("number", Arguments.INTEGER.create(event))
+            .executes((ctx) => {
+                // Get the player from context
+                const player = ctx.source.player;
+                const value = Arguments.INTEGER.getResult(ctx, 'number');
+                // Send confirmation message
+                palladium.setProperty(player, "satsu_iron_man_nano_counts", value);
+
+                return value;
+            })
+        )
+  );
+});
