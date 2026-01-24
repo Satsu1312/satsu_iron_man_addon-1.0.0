46efff520418c41e0a29cedbc5c2b43d8c2e44e6
Satsu1312 <minion1312@hotmail.com>
2026-01-24 09:07:38 -0600

    feature: Quantum suit first stuff

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -280,6 +280,13 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
     }
 });
 PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player" || "palladium:custom_projectile") {
+      // Arguments: Key of the property, type of the property, default/starting value
+      event.registerProperty('satsu_iron_man_quantum_suit', 'string', "off");
+    }
+});
+PalladiumEvents.registerPropertiesClientSided((event) => {
   event.registerProperty(
     "satsu_iron_man_addon.PrimaryColour",
     "integer",
