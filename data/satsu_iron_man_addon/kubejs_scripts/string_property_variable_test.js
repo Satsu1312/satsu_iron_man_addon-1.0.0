4fb7f8a4af2578cf172b47c2fa76f70b2310bcb6
Satsu1312 <minion1312@hotmail.com>
2026-01-24 22:10:15 -0600

    Rescue

--- data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ data/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -293,6 +293,13 @@ PalladiumEvents.registerProperties((event) => {
   }
 });
 PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player" || "palladium:custom_projectile") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_mark_49_energy_deploy", "string", "off");
+  }
+});
+PalladiumEvents.registerProperties((event) => {
   event.registerProperty(
     "satsu_iron_man_addon.PrimaryColour",
     "integer",
