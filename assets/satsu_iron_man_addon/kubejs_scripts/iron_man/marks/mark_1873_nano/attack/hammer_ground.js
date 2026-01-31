7c9f1a612e150482db68863b74190505938b24e6
Satsu1312 <minion1312@hotmail.com>
2026-01-31 12:37:00 -0600

    New attack all nano

---
+++ assets/satsu_iron_man_addon/kubejs_scripts/iron_man/marks/mark_1873_nano/attack/hammer_ground.js
@@ -0,0 +1,149 @@
+// Register custom animations
+PalladiumEvents.registerAnimations((event) => {
+  event.registerForPower(
+    "satsu_iron_man_addon/marks/mark_1873_nano/nano_mallet.ground_attack",
+        "satsu_iron_man_addon:iron_man/marks/mark_1873_nano/main",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:iron_man/marks/mark_1873_nano/main",
+        "nano_mallet.ground_attack.animation",
+        builder.getPartialTicks()
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(0)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(0)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("left_arm")
+            .setXRotDegrees(-126.2857)
+            .setYRotDegrees(-14.2601)
+            .setZRotDegrees(10.251)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-126.2857)
+            .setYRotDegrees(14.2601)
+            .setZRotDegrees(-10.251)
+            .moveX(0)
+            .moveY(0)
+            .moveZ(0)
+            .animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
+  event.registerForPower(
+    "satsu_iron_man_addon/marks/mark_1873_nano/nano_mallet.ground_attack.2",
+        "satsu_iron_man_addon:iron_man/marks/mark_1873_nano/main",
+    10,
+    (builder) => {
+      // Gets the current animation timer progress from the ability, returned value is a number from 0.0 to 1.0
+      const progress = animationUtil.getAnimationTimerAbilityValue(
+        builder.getPlayer(),
+        "satsu_iron_man_addon:iron_man/marks/mark_1873_nano/main",
+        "nano_mallet.ground_attack.animation_2",
+        builder.getPartialTicks()
+      );
+
+      // only apply animation if progress is above 0!
+      if (progress > 0.0) {
+        const halfPi = 1.57079632679;
+
+        // if first person
+        if (builder.isFirstPerson()) {
+          builder
+            .get("left_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(0)
+            .animate("InOutCubic", progress);
+        }
+        if (builder.isFirstPerson()) {
+          builder
+            .get("right_arm")
+            .setXRotDegrees(0)
+            .setYRotDegrees(0)
+            .setZRotDegrees(0)
+            .animate("InOutCubic", progress);
+        } else {
+          // third person animations
+          builder
+            .get("left_arm")
+            .setXRotDegrees(-3.7857)
+            .setYRotDegrees(14.2601)
+            .setZRotDegrees(10.251)
+            .moveX(0)
+            .moveY(7.8785)
+            .moveZ(-7)
+            .animate("InOutCubic", progress);
+          builder
+            .get("right_arm")
+            .setXRotDegrees(-3.7857)
+            .setYRotDegrees(14.2601)
+            .setZRotDegrees(-10.251)
+            .moveX(0)
+            .moveY(7.8785)
+            .moveZ(-7)
+            .animate("InOutCubic", progress);
+          builder
+            .get("head")
+            .setXRotDegrees(10)
+            .moveY(10.9693)
+            .moveZ(-9.4488)
+            .animate("InOutCubic", progress);
+          builder
+            .get("chest")
+            .moveY(6.5061)
+            .moveZ(-5.3876)
+            .setXRotDegrees(62.5)
+            .animate("InOutCubic", progress);
+          builder
+            .get("right_leg")
+            .setXRotDegrees(-22.3265)
+            .setYRotDegrees(2.8631)
+            .setZRotDegrees(6.9349)
+            .moveX(0)
+            .moveY(1.9924)
+            .moveZ(0.8257)
+            .animate("InOutCubic", progress);
+          builder
+            .get("left_leg")
+            .setXRotDegrees(28.493)
+            .setYRotDegrees(-10.5453)
+            .setZRotDegrees(-10.7286)
+            .moveX(0)
+            .moveY(1.7071)
+            .moveZ(0.7071)
+            .animate("InOutCubic", progress);
+          builder;
+        }
+      }
+    }
+  );
+});
