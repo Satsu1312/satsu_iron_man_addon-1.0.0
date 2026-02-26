7f731ad722475953031b475a65b8891115c4c65c
Satsu1312 <minion1312@hotmail.com>
2026-02-26 10:21:26 -0600

    Edited

--- addon/satsu_iron_man_addon/kubejs_scripts/abilities/effect_imnunity.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/abilities/effect_imnunity.js
@@ -8,19 +8,11 @@ StartupEvents.registry("palladium:abilities", (event) => {
       "minecraft:poison",
       "The effect the entity is immune to"
     )
-    .addProperty(
-      "command",
-      "string",
-      "",
-      "The command to run when the entity is given the effect"
-    )
     .tick((entity, entry, holder, enabled) => {
-      const command = entry.getPropertyByName("command");
       const effect = entry.getPropertyByName("effect");
       if (enabled) {
         if (entity.hasEffect(effect)) {
           entity.removeEffect(effect);
-          entity.runCommandSilent(command);
         }
       }
     });
