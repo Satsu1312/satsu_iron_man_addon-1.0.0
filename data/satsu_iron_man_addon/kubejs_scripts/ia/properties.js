d8fff80563981317de2e9acbc7bd355d3012b24d
Satsu1312 <minion1312@hotmail.com>
2026-03-10 18:31:13 -0600

    IA function fixed

--- data/satsu_iron_man_addon/kubejs_scripts/ia/properties.js
+++ data/satsu_iron_man_addon/kubejs_scripts/ia/properties.js
@@ -26,3 +26,10 @@ PalladiumEvents.registerProperties((event) => {
     event.registerProperty("satsu_ia_wheel.recharge_mode", "string", "false");
   }
 });
+PalladiumEvents.registerProperties((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_ia_wheel.stuff", "string", "none");
+  }
+});
