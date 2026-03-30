bb6aebee61829ab9c007111664234a2ab96ed4c2
Satsu1312 <minion1312@hotmail.com>
2026-03-30 12:44:09 -0600

    Update satsu_set_item_nbt.js

--- data/satsu_iron_man_addon/kubejs_scripts/commands/satsu_set_item_nbt.js
+++ data/satsu_iron_man_addon/kubejs_scripts/commands/satsu_set_item_nbt.js
@@ -5,14 +5,27 @@ ServerEvents.commandRegistry((e) => {
       C.argument("slot", A.STRING.create(e)).then(
         C.argument("nbtKey", A.STRING.create(e)).then(
           C.argument("nbtValue", A.STRING.create(e)).executes((ctx) => {
-            const p = ctx.source.player,
-              s = A.STRING.getResult(ctx, "slot"),
-              k = A.STRING.getResult(ctx, "nbtKey"),
-              v = A.STRING.getResult(ctx, "nbtValue"),
-              it = p.getItemBySlot(s);
-            if (!it || it.isEmpty())
+            const p = ctx.source.player;
+            let s = A.STRING.getResult(ctx, "slot");
+            const k = A.STRING.getResult(ctx, "nbtKey");
+            const v = A.STRING.getResult(ctx, "nbtValue");
+
+            // Si el slot empieza con "curios_", lo convertimos a curios:slot
+            if (s.startsWith("curios_")) {
+              s = "curios:" + s.substring(7);
+            }
+
+            const stacksOrItem = global.getItemFromSlot(p, s);
+            if (!stacksOrItem)
               return (p.tell(Text.red("No hay ítem en ese slot.")), 0);
-            let nbt = it.nbt || {};
+
+            const item = s.startsWith("curios:")
+              ? stacksOrItem.getStackInSlot(0)
+              : stacksOrItem;
+            if (!item || item.isEmpty())
+              return (p.tell(Text.red("No hay ítem en ese slot.")), 0);
+
+            let nbt = item.nbt || {};
 
             if (k === "Enchantments") {
               let ench = nbt.Enchantments || [];
@@ -34,11 +47,9 @@ ServerEvents.commandRegistry((e) => {
               else nbt[k] = v;
             }
 
-            const newIt = it.withNBT(nbt),
-              map = { feet: 36, legs: 37, chest: 38, head: 39 };
-            map[s]
-              ? p.inventory.setItem(map[s], newIt)
-              : p.setItemSlot(s, newIt);
+            const newIt = item.withNBT(nbt);
+            global.setItemInSlot(p, s, newIt);
+
             return 1;
           }),
         ),
