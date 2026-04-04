7b3c97b7e315fe780023358345531c8a4b920fe1
Satsu1312 <minion1312@hotmail.com>
2026-04-04 12:10:01 -0600

    Other property

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -532,6 +532,13 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
   }
 });
 PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player" || "palladium:custom_projectile") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_strenght_power", "string", "normal");
+  }
+});
+PalladiumEvents.registerPropertiesClientSided((event) => {
   event.registerProperty(
     "satsu_iron_man_addon.PrimaryColour",
     "integer",
