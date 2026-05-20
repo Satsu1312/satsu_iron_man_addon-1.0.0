e4b44c830ed98bdd921a5e99272f1cf813c247cc
Satsu1312 <minion1312@hotmail.com>
2026-05-20 09:55:04 -0600

    heat first stuff

--- assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
+++ assets/satsu_iron_man_addon/kubejs_scripts/string_property_variable_test.js
@@ -602,6 +602,13 @@ PalladiumEvents.registerPropertiesClientSided((event) => {
     }
 });
 PalladiumEvents.registerPropertiesClientSided((event) => {
+  // Only register for players
+  if (event.getEntityType() === "minecraft:player") {
+    // Arguments: Key of the property, type of the property, default/starting value
+    event.registerProperty("satsu_iron_man_addon_heat_repulsor", 'integer', 0);
+  }
+});
+PalladiumEvents.registerPropertiesClientSided((event) => {
   event.registerProperty(
     "satsu_iron_man_addon.PrimaryColour",
     "integer",
