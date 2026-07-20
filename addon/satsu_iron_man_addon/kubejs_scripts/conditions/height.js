66adf19b54ea52605426762cd84e157043b4afac
Satsu1312 <minion1312@hotmail.com>
2026-07-20 16:40:01 -0600

    Mark 21 more damage with the height

--- addon/satsu_iron_man_addon/kubejs_scripts/conditions/height.js
+++ addon/satsu_iron_man_addon/kubejs_scripts/conditions/height.js
@@ -2,11 +2,16 @@ StartupEvents.registry("palladium:condition_serializer", (event) => {
   event
     .create("satsy_iron_man_addon:height_bool_condition")
 
-    //property
-    .addProperty("max_height", "integer", 100, "how high you can go")
+    // Propiedades de altura mínima y máxima
+    .addProperty("min_height", "integer", -64, "how low you can go")
+    .addProperty("max_height", "integer", 320, "how high you can go")
 
     .test((entity, properties) => {
-      const height = properties.get("max_height");
-      return entity.getY() >= height;
+      const minHeight = properties.get("min_height");
+      const maxHeight = properties.get("max_height");
+      const currentY = entity.getY();
+
+      // Comprueba que la entidad esté DENTRO del rango [minHeight, maxHeight]
+      return currentY >= minHeight && currentY <= maxHeight;
     });
 });
